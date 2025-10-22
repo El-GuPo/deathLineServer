35b02c56a32482bd185cd2781b7eca5ded8709e0
a.podmarev <a.podmarev@vkteam.ru>
2025-10-22 15:13:16 +0300

    initial commit

---
+++ src/main/java/org/elgupo/deathlineserver/DeathLineServerApplication.java
@@ -0,0 +1,13 @@
+package org.elgupo.deathlineserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeathLineServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeathLineServerApplication.class, args);
+	}
+
+}
